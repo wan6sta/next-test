6c1e6b546aefaec362d82bb8e916333deed61e1d
Korablin Ivan <wan6sta@gmail.com>
2022-07-28 19:54:25 +0300

    first

---
+++ app/components/screens/home/Home.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { IHome } from './home.interface'
+
+const Home: FC<IHome> = () => {
+	return (
+		<div>
+			<h1>Home page</h1>
+		</div>
+	)
+}
+
+export default Home
